c736b3c2fc0f8d3c8b8b878064c8822ad04b206f
Arinana <allyut12zk@gmail.com>
2026-02-10 19:14:23 +0300

    Update Song.cs

--- Models/Song.cs
+++ Models/Song.cs
@@ -11,7 +11,7 @@
         public List<string> Notes { get; set; } = new();
         public int Duration { get; set; }
         public string Review { get; set; } = "";
-        public string? CoverImageBase64 { get; set; }; // обложка
+        public string? CoverImageBase64 { get; set; }
         public byte[] AudioPreview { get; set; } = Array.Empty<byte>(); // mp3/миди превью
         public List<string> Lyrics { get; set; } = new(); // для синхронного скролла
     }
