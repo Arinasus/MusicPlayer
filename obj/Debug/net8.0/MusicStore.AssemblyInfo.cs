177c67115bb977812f868c82b41845a2cd7e2048
Arinana <allyut12zk@gmail.com>
2026-02-10 19:59:19 +0300

    Добавление библиотеки

--- obj/Debug/net8.0/MusicStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/MusicStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b74dc401b11c8bc0b6cf2a80d537456a61e2eae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc790810da0a975a8902e4e88e8a55c8b7929617")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
